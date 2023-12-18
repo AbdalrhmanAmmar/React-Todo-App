2803d8582680ae0581af8f106f74395601635adf
AbdalrhmanAmmar <bodyty2n@gmail.com>
2023-12-18 08:36:42 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -36,11 +36,11 @@ function App() {
 
   const UpdateTask = (id) => {
     setTypeButton(false);
-     setSelectedTask(id);
+    setSelectedTask(id);
   };
 
   return (
-    <div className="grid grid-cols-12 gap-6 px-5 xl:pr-40 my-14 sm:px-20 md:px-40px  flex-wrap-reverse ">
+    <div className="grid grid-cols-12 gap-6 px-5 xl:pr-40 my-14 sm:px-20 md:px-40px  flex-wrap-reverse border-none ">
       <div className="col-span-12 p-4 text-center  xl:col-span-4 md:col-span-12  rounded-2xl bg-white sidebar xl:order-2 xl:w-[120%]">
         <TodoApp
           Task={Task}
